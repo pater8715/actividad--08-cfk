45cd029800447842bfc1fe3bcd2495a074103a37
pater8715 <pater8715@gmail.com>
2022-09-19 09:06:52 -0500

    Final

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
 let imc = 0
-let estatura = 0
-let peso = 0
+let estatura = 180
+let peso = 50
 basic.forever(function () {
-    imc = 0
-    estatura = 180
-    peso = 74
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.buttonIsPressed(Button.AB)) {
+        imc += 10000 * peso / estatura ** 2
+        basic.showNumber(imc)
+        if (imc < 18.5) {
+            basic.showString("Delgado")
+        } else if (imc > 18.5 && imc <= 25) {
+            basic.showString("Saludable")
+        } else if (imc > 25 && imc <= 30) {
+            basic.showString("Sobrepeso")
+        } else {
+            basic.showString("Obesidad")
+        }
+    } else if (input.buttonIsPressed(Button.A)) {
         peso = peso + 1
         basic.showNumber(peso)
     } else if (input.buttonIsPressed(Button.B)) {
-        peso += peso - 1
+        peso = peso - 1
         basic.showNumber(peso)
-    } else if (input.buttonIsPressed(Button.AB)) {
-    	
     } else if (input.isGesture(Gesture.Shake)) {
-    	
+        peso = 74
     }
 })
